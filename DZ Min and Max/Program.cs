2047c48abe3fac6b7aac0666249b385129964465
Andrey Bukatish <poetrade@gmail.com>
2023-03-09 23:21:22 +0300

    Добавил ГИТ игнор

---
+++ DZ Min and Max/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите 2 числа!");
+int a = Convert.ToInt32( Console.ReadLine());
+int b = Convert.ToInt32( Console.ReadLine());
+
+if (a > b)  {
+    
+    Console.WriteLine( " Первое число больше"); Console.Write (a);
+}
+else
+{
+    
+    Console.WriteLine( "Второе число больше") ; Console.Write (b);
+}
+
