91a9b82acea99e71bdb38b84f5aa011f0268dcbd
Andrey Bukatish <poetrade@gmail.com>
2023-03-10 19:52:44 +0300

    Написал програму Мах из 3 чисел

---
+++ DZ_max_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа!");
+
+int a = Convert.ToInt32( Console.ReadLine());
+int b = Convert.ToInt32( Console.ReadLine());
+int c = Convert.ToInt32( Console.ReadLine());
+int max = a;
+
+if (b > max){ b=max;
+}  if(c > max){ c = max;
+}
+Console.WriteLine(" Это самое большое число!");Console.Write(max);
