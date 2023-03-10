6a1b51448bbbbf8e738156985fd08d9667ac2ab8
Andrey Bukatish <poetrade@gmail.com>
2023-03-10 21:50:10 +0300

    написал программу Max из 3х чисел Fix

--- DZ_max_3/Program.cs
+++ DZ_max_3/Program.cs
@@ -2,12 +2,36 @@
 
 Console.WriteLine("Введите три числа!");
 
-int a = Convert.ToInt32( Console.ReadLine());
-int b = Convert.ToInt32( Console.ReadLine());
-int c = Convert.ToInt32( Console.ReadLine());
-int max = a;
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 
-if (b > max){ b=max;
-}  if(c > max){ c = max;
+ int max;
+ if (a >= b)
+ {
+    if (c >= a)
+    {
+       max = c;
+    }
+    else
+    {
+       max = a;
+    }
+ }
+ else
+ {
+    if (c >= b)
+    {
+       max = c;
+    }
+    else
+    {
+       max = b;
+    }
+ }
+
+
+Console.Write(" Это самое большое число!");Console.Write(max);
+{
+    
 }
-Console.WriteLine(" Это самое большое число!");Console.Write(max);
